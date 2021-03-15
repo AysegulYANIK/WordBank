18cf21807a3ab7d7c325aef3e8ab63b0a7218181
Ayşegül YANIK <aysegul_demirtas@hotmail.com.tr>
2021-03-15 17:47:57 +0300

    tables
    sql sm tables for words

---
+++ WordBank.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].WordBank
+(
+   word VARCHAR (100) NOT NULL PRIMARY KEY,
+   phonetics VARCHAR(100),
+   w_type VARCHAR(100),
+   w_level VARCHAR(100) NULL,
+   meaning VARCHAR(500) NULL,
+   collocations VARCHAR(500) NULL,
+   intentence VARCHAR(500) NULL,
+   synonym_s VARCHAR(500)
+)
---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿create table WordsList(
+   word VARCHAR (100) NOT NULL,
+   phonetics VARCHAR(100),
+   w_level VARCHAR(100) NOT NULL,
+   meaning VARCHAR(500) NOT NULL,
+   collocations VARCHAR(500) NOT NULL,
+   intentence VARCHAR(500) NOT NULL,
+   synonym_s VARCHAR(500),
+   PRIMARY KEY(word)
+);
